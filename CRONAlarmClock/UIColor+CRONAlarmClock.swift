d006f019802223545b7939fbe8f5bba1c4a37972
Zach Churchill <zachurchill@protonmail.com>
2020-09-30 19:51:39 -0400

    Set up project colors and UIColor extension

---
+++ CRONAlarmClock/UIColor+CRONAlarmClock.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+CRONAlarmClock.swift
+//  CRONAlarmClock
+//
+//  Created by Zachary Churchill on 9/30/20.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let accent = UIColor(named: "AccentColor")!
+    static let light = UIColor(named: "LightColor")!
+    static let mediumLight = UIColor(named: "MediumLightColor")!
+    static let medium = UIColor(named: "MediumColor")!
+    static let dark = UIColor(named: "DarkColor")!
+}
